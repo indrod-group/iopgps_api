73f8dbb1309056a32b94fe3ae2fd79c660f22585
Christopher Villamarín <alexpila.314@gmail.com>
2024-01-17 19:13:33 -0500

    feat(settings): Integrate Redis as cache in the API
    The Redis instance is obtained from the environment variables. This feature improves performance by storing frequently used data in the Redis cache.

--- wt_iopgps/settings.py
+++ wt_iopgps/settings.py
@@ -107,6 +107,19 @@ DATABASES = {
     )
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv('REDIS_LOCATION', 'redis://127.0.0.1:6379/1'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
