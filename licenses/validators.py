89ad8fb760fdd9107b4a983d1018271992652f8d
Christopher Villamarín <alexpila.314@gmail.com>
2024-01-30 17:36:48 -0500

    fix(licenses): validate expire_date and issue_date

--- licenses/validators.py
+++ licenses/validators.py
@@ -1,8 +1,8 @@
-from datetime import date, datetime
+from datetime import date
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-def validate_license_validity(expiry_date: str):
+def validate_license_validity(expiry_date: date):
     """
     Validates the validity of a license based on its expiry date.
 
@@ -12,13 +12,13 @@ def validate_license_validity(expiry_date: str):
     Raises:
         - ValidationError: If the license has expired.
     """
-    if datetime.fromisoformat(expiry_date) <= date.today():
+    if expiry_date <= date.today():
         raise ValidationError(
             f"{expiry_date} is not a valid date",
             params={'value': expiry_date}
         )
 
-def validate_issue_date(issue_date: str):
+def validate_issue_date(issue_date: date):
     """
     Validates the issue date of a license.
 
@@ -28,7 +28,7 @@ def validate_issue_date(issue_date: str):
     Raises:
         - ValidationError: If the issue date is in the future.
     """
-    if datetime.fromisoformat(issue_date) >= date.today():
+    if issue_date >= date.today():
         raise ValidationError(
             f"{issue_date} is not a valid date. The issue date cannot be in the future.",
             params={'value': issue_date}
