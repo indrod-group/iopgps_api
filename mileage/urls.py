4a38a5a6ec12b220173b452ca91ad9124aa6e2a3
Christopher Villamarín <alexpila.314@gmail.com>
2024-02-27 01:47:48 -0500

    feat(mileage): create api routes for mileage objects

--- mileage/urls.py
+++ mileage/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MileageViewSet
+from .views import MileageViewSet, VehicleMileageReadAndCreate
+
 
 router = DefaultRouter()
-router.register(r'mileages', MileageViewSet)
+router.register(r'mileages', MileageViewSet, basename='mileages')
+
+router2 = DefaultRouter()
+router2.register(r'mileages', VehicleMileageReadAndCreate, basename='vehicles')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('vehicles/<str:vuid>/', include(router2.urls)),
 ]
--- mileage/views.py
+++ mileage/views.py
@@ -1,7 +1,30 @@
+from typing import Optional
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+
+from vehicles.models import Vehicle
 from .models import Mileage
 from .serializers import MileageSerializer
 
 class MileageViewSet(viewsets.ModelViewSet):
     queryset = Mileage.objects.all()
     serializer_class = MileageSerializer
+
+class VehicleMileageReadAndCreate(
+    viewsets.ReadOnlyModelViewSet,
+    viewsets.mixins.CreateModelMixin
+):
+    serializer_class = MileageSerializer
+
+    def get_queryset(self):
+        """
+        This method retrieves the queryset of Mileage objects
+        for the vehicle specified by the 'pk' parameter in the URL,
+        which corresponds to the 'vuid' of the vehicle.
+        If the 'vuid' is None or does not exist, it returns an empty queryset.
+        """
+        vuid: Optional[str] = self.kwargs.get("vuid")
+        if vuid is not None:
+            vehicle = get_object_or_404(Vehicle, vuid=vuid)
+            return Mileage.objects.filter(vehicle=vehicle)
+        return Mileage.objects.none()
