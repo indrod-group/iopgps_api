a44c5ad3af576ef15f54989cf2b77012ce96a880
Christopher Villamarín <alexpila.314@gmail.com>
2024-03-26 13:11:40 -0500

    feat(admin-panel): customize Django admin panel
    Updated Django admin panel with custom title, branding, and added a logo image. Also included custom CSS for styling.

--- wt_iopgps/settings.py
+++ wt_iopgps/settings.py
@@ -119,7 +119,7 @@ ROOT_URLCONF = "wt_iopgps.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'users/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -187,10 +187,10 @@ USE_TZ = True
 # This setting tells Django at which URL static files are going to be served to the user.
 # Here, they well be accessible at your-domain.onrender.com/static/...
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
